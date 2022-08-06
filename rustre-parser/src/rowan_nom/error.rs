62fd08fabb273189c9c4a6a70a61a5cb95bd8e21
Edgar Onghena <dev@edgar.bzh>
2022-08-06 23:01:22 +0200

    Hotfix

--- rustre-parser/src/rowan_nom/error.rs
+++ rustre-parser/src/rowan_nom/error.rs
@@ -13,3 +13,30 @@ pub trait RowanNomError<Lang: super::Language> {
 
     // TODO add much more errors, include location information in constructors
 }
+
+/// A ZST that implements [`RowanNomError`] when details or context don't matter
+#[derive(Debug, Default, Clone, Copy, thiserror::Error)]
+#[error("dummy error used, can't provide further information")]
+pub struct DummyError;
+
+impl<Lang: super::Language> RowanNomError<Lang> for DummyError {
+    fn from_message(_message: &str) -> Self {
+        Self
+    }
+
+    fn from_unexpected_eof(_position: usize) -> Self {
+        Self
+    }
+
+    fn from_unexpected_token(
+        _span: Range<usize>,
+        _expected: Lang::Kind,
+        _found: Lang::Kind,
+    ) -> Self {
+        Self
+    }
+
+    fn with_context(self, _ctx: &'static str) -> Self {
+        Self
+    }
+}
